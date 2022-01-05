fa3626cc223d10e05e8100ac980abde61519a240
itsirajul <shirajulislamparvez@gmail.com>
2022-01-05 21:29:42 +0600

    Initial commit

---
+++ android/app/src/main/java/com/example/layout_challenge_reverse/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.layout_challenge_reverse;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100.0,
+                color: Colors.red,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:<Widget> [
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.blue,
+              )
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
+
